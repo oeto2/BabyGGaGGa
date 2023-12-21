98f971b63c14972629c0c5577de61b128dbee427
sskesu <136444697+sskesu@users.noreply.github.com>
2023-12-21 19:21:32 +0900

    startScene 1차

---
+++ Assets/Scripts/diffBtn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diffBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void diffChange()
+    {
+        if (transform.Find("Normal").gameObject.activeSelf)
+        {
+            transform.Find("Normal").gameObject.SetActive(false);
+            transform.Find("Hard").gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.Find("Normal").gameObject.SetActive(true);
+            transform.Find("Hard").gameObject.SetActive(false);
+        }
+    }
+}
