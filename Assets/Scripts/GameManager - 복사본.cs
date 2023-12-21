3669573db4a10c0a6f55224449092cd9670359d4
sda0503 <sda0503@naver.com>
2023-12-21 15:42:20 +0900

    업로드 테스트

---
+++ Assets/Scripts/GameManager - 복사본.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public Text timeText;
+    public GameObject endText;
+    public GameObject card;
+    float time;
+    float item;
+    public GameObject firstCard;
+    public GameObject secondCard;
+
+    public AudioSource audioSource;
+    public AudioClip match;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if(instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+
+        int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject newCard = Instantiate(card);
+            newCard.transform.parent = GameObject.Find("Cards").transform;
+            float x = (i / 4) * 1.4f - 2.1f;
+            float y = (i % 4) * 1.4f - 3.0f;
+
+            newCard.transform.position = new Vector3(x, y, 0);
+
+            string rtanName = "rtan" + rtans[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(rtanName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeText.text = time.ToString("N2");
+
+        //게임 오버
+        if(time >= 3f)
+        {
+            GameEnd();
+        }
+    }
+
+    public void IsMatched()
+    {
+        string firstCardImage = firstCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+        string secondCardImage = secondCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+
+
+        if(firstCardImage == secondCardImage)
+        {
+            audioSource.PlayOneShot(match);
+
+            firstCard.GetComponent<card>().DestroyCard();
+            secondCard.GetComponent<card>().DestroyCard();
+
+            int cardsLeft = GameObject.Find("Cards").transform.childCount;
+            Debug.Log(GameObject.Find("Cards").transform.childCount);
+            if (cardsLeft == 2)
+            {
+                //종료시키자!!
+                Time.timeScale = 0f;
+                endText.SetActive(true);
+                //Invoke("GameEnd", 1f);
+            }
+        }
+        else
+        {
+            firstCard.GetComponent<card>().CloseCard();
+            secondCard.GetComponent<card>().CloseCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
+    }
+
+    void GameEnd()
+    {
+        //종료시키자!!
+        Time.timeScale = 0f;
+        endText.SetActive(true);
+    }
+
+    public void RetryGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
