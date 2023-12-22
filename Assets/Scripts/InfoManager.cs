4061b644f53d5e2c2122424d51fa33554d997272
oeto2 <oeto2@naver.com>
2023-12-22 17:35:37 +0900

    인포 매니저 언락 구현중

--- Assets/Scripts/InfoManager.cs
+++ Assets/Scripts/InfoManager.cs
@@ -2,27 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[System.Serializable]
+public class UnlockInfo
+{
+    //해금할 멤버 
+    public bool cch1 = false;
+    public bool cch2 = false;
+    public bool pjh1 = false;
+    public bool pjh2 = false;
+    public bool yjh1 = false;
+    public bool yjh2 = false;
+    public bool lsm1 = false;
+    public bool lsm2 = false;
+    public bool ljh1 = false;
+    public bool ljh2 = false;
+}
 
 public class InfoManager : MonoBehaviour
 {
     //싱글톤
     public static InfoManager instance = null;
 
+    //배경음 크기
+    public float bgmValume = 1f;
+
+    //효과음 크기
+    public float effectValume = 1f;
+
+    //난이도
+    public int int_level = 0;
+
+    //멤버 정보값 클래스
+    public UnlockInfo unlockInfo;
+
     private void Awake()
     {
-        if(instance != null)
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(this.gameObject);
         }
         else
         {
-            if(instance != this)
+            if (instance != this)
             {
                 Destroy(this.gameObject);
             }
         }
     }
-    
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            unlockInfo.cch1 = true;
+        }
+    }
 }
