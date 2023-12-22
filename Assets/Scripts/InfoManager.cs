ee9c3c17813a4d4b0fbbc8c741d2550a4f6469cb
oeto2 <oeto2@naver.com>
2023-12-22 20:22:24 +0900

    언락인포 타입 수정
    class bool -> bool[]로 수정

--- Assets/Scripts/InfoManager.cs
+++ Assets/Scripts/InfoManager.cs
@@ -2,21 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class UnlockInfo
-{
-    //해금할 멤버 
-    public bool cch1 = false;
-    public bool cch2 = false;
-    public bool pjh1 = false;
-    public bool pjh2 = false;
-    public bool yjh1 = false;
-    public bool yjh2 = false;
-    public bool lsm1 = false;
-    public bool lsm2 = false;
-    public bool ljh1 = false;
-    public bool ljh2 = false;
-}
 
 public class InfoManager : MonoBehaviour
 {
@@ -32,8 +17,8 @@ public class InfoManager : MonoBehaviour
     //난이도
     public int int_level = 0;
 
-    //멤버 정보값 클래스
-    public UnlockInfo unlockInfo;
+    //멤버 정보값 배열
+    public bool[] unlockInfo;
 
     private void Awake()
     {
@@ -49,10 +34,5 @@ public class InfoManager : MonoBehaviour
                 Destroy(this.gameObject);
             }
         }
-    }
-
-    private void Update()
-    {
-    
     }
 }
